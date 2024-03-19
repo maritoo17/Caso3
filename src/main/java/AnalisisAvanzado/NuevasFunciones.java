44e1af9ee67869dd11830b449089e38ac8dc50fa
maritoo17 <msancgim@myuax.com>
2024-03-19 11:05:10 +0100

    32

--- src/main/java/AnalisisAvanzado/NuevasFunciones.java
+++ src/main/java/AnalisisAvanzado/NuevasFunciones.java
@@ -14,6 +14,18 @@ public class NuevasFunciones {
         this.funciones.add(funcion);
     }
 
+    public FuncionPersonalizada obtenerFuncion(int indice) {
+        return this.funciones.get(indice);
+    }
+
+    public void eliminarFuncion(int indice) {
+        this.funciones.remove(indice);
+    }
+
+    public void limpiarFunciones() {
+        this.funciones.clear();
+    }
+
     public double aplicarFunciones(double input) {
         double resultado = input;
         for (FuncionPersonalizada funcion : funciones) {
