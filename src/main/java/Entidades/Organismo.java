21cc61b0ae8bd909288eaf24f5bbe9e267eea73e
maritoo17 <msancgim@myuax.com>
2024-03-19 19:48:55 +0100

    51

--- src/main/java/Entidades/Organismo.java
+++ src/main/java/Entidades/Organismo.java
@@ -1,18 +1,20 @@
 package Entidades;
 
 public abstract class Organismo {
+    protected String nombre;
     protected int posicionX;
     protected int posicionY;
     protected int salud;
     protected int edad;
     protected boolean esReproductivo;
 
-    public Organismo(int posicionX, int posicionY, int salud, int edad, boolean esReproductivo) {
+    public Organismo(int posicionX, int posicionY, int salud, int edad, boolean esReproductivo, String nombre) {
         this.posicionX = posicionX;
         this.posicionY = posicionY;
         this.salud = salud;
         this.edad = edad;
         this.esReproductivo = esReproductivo;
+        this.nombre = nombre;
     }
 
     public abstract void mover();
@@ -27,4 +29,13 @@ public abstract class Organismo {
     public void setSalud(int salud) {
         this.salud = salud;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
 }
