18a0852fbda0c57bc8ddf4d20700de95bea0ec83
maritoo17 <msancgim@myuax.com>
2024-03-19 19:49:02 +0100

    52

--- src/main/java/DinamicasPoblacionales/EventoAleatorio.java
+++ src/main/java/DinamicasPoblacionales/EventoAleatorio.java
@@ -21,15 +21,19 @@ public class EventoAleatorio {
             case "desastre natural":
                 animal.setSalud(animal.getSalud() - 10);
                 ambiente.setRecursosDisponibles (ambiente.getRecursosDisponibles()- 20);
+                System.out.println("Desastre natural: los recursos disponibles en el ambiente disminuyeron en 20 unidades y la salud del animal disminuyó en 10 unidades.");
                 break;
             case "enfermedad":
-                animal.setSalud(animal.getSalud() - 20);
+                animal.setSalud(animal.getSalud() - 5);
+                System.out.println("Enfermedad: la salud del animal disminuyó en 5 unidades.");
                 break;
             case "cambio climático":
                 ambiente.setRecursosDisponibles(ambiente.getRecursosDisponibles() - 30);
+                System.out.println("Cambio climático: los recursos disponibles en el ambiente disminuyeron en 30 unidades.");
                 break;
             case "cambio de clima":
                 ambiente.cambiarClima();
+                System.out.println("Cambio de clima: el clima del ambiente ha cambiado.");
                 break;
             default:
                 throw new IllegalArgumentException("Evento no válido");
