d44bf4a50c75a48c668b3b7b3136fc9a10dbae62
maritoo17 <msancgim@myuax.com>
2024-03-19 10:25:55 +0100

    23

---
+++ src/main/java/UsuariosYSimulaciones/Simulacion.java
@@ -0,0 +1,21 @@
+package UsuariosYSimulaciones;
+
+public class Simulacion {
+    private String estado;
+
+    public Simulacion() {
+        this.estado = "No iniciada";
+    }
+
+    public void iniciarSimulacion() {
+        this.estado = "Iniciada";
+    }
+
+    public void ejecutarSimulacion() {
+        this.estado = "Ejecutada";
+    }
+
+    public String obtenerResultados() {
+        return "Resultados de la simulación: " + this.estado;
+    }
+}
