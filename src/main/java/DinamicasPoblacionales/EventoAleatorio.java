1a6a698db2bb73dc3c02f90cb5aa24931757f322
maritoo17 <msancgim@myuax.com>
2024-03-20 13:42:40 +0100

    57

--- src/main/java/DinamicasPoblacionales/EventoAleatorio.java
+++ src/main/java/DinamicasPoblacionales/EventoAleatorio.java
@@ -1,8 +1,8 @@
 package DinamicasPoblacionales;
 
-import Entidades.*;
-
 import java.util.Random;
+
+import Entidades.*;
 
 public class EventoAleatorio {
     private String[] eventos = {"desastre natural", "enfermedad", "cambio climático"};
@@ -13,30 +13,28 @@ public class EventoAleatorio {
         return eventos[indice];
     }
 
-    public void aplicarEvento(String evento, Animal animal, Ambiente ambiente) {
+    public String aplicarEvento(String evento, Animal animal, Ambiente ambiente) {
         if (animal == null || ambiente == null) {
             throw new IllegalArgumentException("El animal y el ambiente no pueden ser nulos");
         }
+        String mensajeEvento = "";
         switch (evento) {
             case "desastre natural":
                 animal.setSalud(animal.getSalud() - 10);
-                ambiente.setRecursosDisponibles (ambiente.getRecursosDisponibles()- 20);
-                System.out.println("Desastre natural: los recursos disponibles en el ambiente disminuyeron en 20 unidades y la salud del animal disminuyó en 10 unidades.");
+                ambiente.setRecursosDisponibles(ambiente.getRecursosDisponibles() - 20);
+                mensajeEvento = "Desastre natural: los recursos disponibles en el ambiente disminuyeron en 20 unidades y la salud del animal disminuyó en 10 unidades.";
                 break;
             case "enfermedad":
                 animal.setSalud(animal.getSalud() - 5);
-                System.out.println("Enfermedad: la salud del animal disminuyó en 5 unidades.");
+                mensajeEvento = "Enfermedad: la salud del animal disminuyó en 5 unidades.";
                 break;
             case "cambio climático":
                 ambiente.setRecursosDisponibles(ambiente.getRecursosDisponibles() - 30);
-                System.out.println("Cambio climático: los recursos disponibles en el ambiente disminuyeron en 30 unidades.");
-                break;
-            case "cambio de clima":
-                ambiente.cambiarClima();
-                System.out.println("Cambio de clima: el clima del ambiente ha cambiado.");
+                mensajeEvento = "Cambio climático: los recursos disponibles en el ambiente disminuyeron en 30 unidades.";
                 break;
             default:
                 throw new IllegalArgumentException("Evento no válido");
         }
+        return mensajeEvento;
     }
 }
