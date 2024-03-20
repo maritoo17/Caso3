47f71358bba7815b0df222c033ec5c75385f0624
maritoo17 <msancgim@myuax.com>
2024-03-20 14:02:16 +0100

    63

--- src/main/java/Ecosistema.java
+++ src/main/java/Ecosistema.java
@@ -27,19 +27,15 @@ public class Ecosistema {
 
             List<Animal> animales = new ArrayList<>();
             animales.add(new Animal("Tigre", 10, 5, 100, 3, true, 100));
-            animales.add(new Animal("Mono", 5, 10, 90, 5, true, 100));
             animales.add(new Animal("Elefante", 0, 0, 80, 7, true, 100));
             animales.add(new Animal("Leon", 0, 5, 100, 4, true, 100));
             animales.add(new Animal("Cebra", 5, 0, 100, 2, true, 100));
-            animales.add(new Animal("Jirafa", 0, 10, 100, 6, true, 100));
 
             List<Planta> plantas = new ArrayList<>();
             plantas.add(new Planta("Margarita", 0, 0, 70, 1, true, 7, 0.05));
             plantas.add(new Planta("Girasol", 0, 0, 70, 1, true, 7, 0.05));
             plantas.add(new Planta("Rosa", 0, 0, 70, 1, true, 7, 0.05));
             plantas.add(new Planta("Tulipán", 0, 0, 70, 1, true, 7, 0.05));
-            plantas.add(new Planta("Orquídea", 0, 0, 70, 1, true, 7, 0.05));
-            plantas.add(new Planta("Lirio", 0, 0, 70, 1, true, 7, 0.05));
 
             CrecimientoYReproduccion crecimiento = new CrecimientoYReproduccion(2, 0.05);
             EventoAleatorio eventoAleatorio = new EventoAleatorio();
@@ -68,42 +64,49 @@ public class Ecosistema {
                         int días = 10;
                         Random random = new Random();
                         for (int i = 0; i < días; i++ ) {
-                            System.out.println ("Día " + (i + 1));
+                            System.out.println("Día " + (i + 1));
 
                             ambiente.cambiarClima();
                             System.out.println("Clima actual: " + ambiente.getClimaActual());
 
-                            String evento = eventoAleatorio.generarEvento();
-                            System.out.println("Evento: " + evento);
-
+                            if (random.nextDouble() < 0.3) {
+                                String evento = eventoAleatorio.generarEvento();
+                                System.out.println("Evento: " + evento);
+                                for (Animal animal : animales) {
+                                    eventoAleatorio.aplicarEvento(evento, animal, ambiente);
+                                }
                             }
+                                for (Animal animal : animales) {
+                                    if (random.nextDouble() < 0.7) {
+                                        animal.mover();
+                                    System.out.println(animal.getNombre() + " se movió a la posición (" + animal.getPosicionX() + ", " + animal.getPosicionY() + ")");
+                                }
 
-                            for (Animal animal : animales) {
-                                animal.mover();
-                                System.out.println(animal.getNombre() + " se movió a la posición (" + animal.getPosicionX() + ", " + animal.getPosicionY() + ")");
+                                if (random.nextDouble() < 0.5) {
+                                    animal.comer(10);
+                                    System.out.println(animal.getNombre() + " ha comido y ha recuperado energía");
+                                }
+                                    if (animal.getEnergia() > 50 && random.nextDouble() < 0.2) {
+                                        System.out.println(animal.getNombre() + " intenta reproducirse.");
+                                    }
 
-                                animal.comer(10);
-                                System.out.println(animal.getNombre() + " ha comido y ha recuperado energía");
-
-                                if (animal.getEnergia() > 50) {
-                                    System.out.println(animal.getNombre() + " intenta reproducirse.");
-                            }
-
-                                animal.envejecer();
-                                System.out.println(animal.getNombre() + " ha envejecido a " + animal.getEdad() + " años.");
-                            }
+                                    animal.envejecer();
+                                    System.out.println(animal.getNombre() + " ha envejecido a " + animal.getEdad() + " años.");
+                                }
 
 
-                            for (Planta planta : plantas) {
-                                planta.envejecer();
-                                System.out.println(planta.getNombre() + " ha envejecido a " + planta.getEdad() + " días.");
+                                for (Planta planta : plantas) {
+                                    planta.envejecer();
+                                    System.out.println(planta.getNombre() + " ha envejecido a " + planta.getEdad() + " días.");
 
-                                planta.reproducirse(planta);
-                                System.out.println(planta.getNombre() + " intenta reproducirse.");
+                                    if (random.nextDouble() < 0.3){
+                                    planta.reproducirse(planta);
+                                    System.out.println(planta.getNombre() + " intenta reproducirse.");
+                                }
                             }
 
                             simulacion.presentarEstadisticas();
-
+                        }
                         simulacion.reset();
                         break;
                     case 3:
